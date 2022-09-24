e27d7112d56dc81405e03da8ab21760aeaa8fa2d
nurmuhammad <juraqobilovnurmuhammad9888@gmail.com>
2022-09-24 16:14:07 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+let elNav = document.querySelector(".js-nav")
+let elBtn = document.querySelector(".btn-js")
+
+elBtn.addEventListener("click", function(){
+    elNav.classList.toggle("js-nav-show")
+    elBtn.classList.toggle("burgerbtn-show")
+})
+
+$(".sleder").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 2,
+    arrows:false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 2220,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow:3
+            }
+        },
+        {
+            breakpoint: 1000,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 620,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '0',
+                slidesToShow:1
+            }
+        }
+    ]
+});
---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors:{
+        yellowOrange: "#F3603C",
+        blueColor: "#242D52",
+        footerBg: "#1D1E25",
+        body: "#F2F2F2",
+        cardBg:"#FFF0EC",
+        colorCarusel:"#fafafa"
+      },
+        backgroundImage:{
+        "overlayGradent":" linear-gradient(180deg, rgba(0, 0, 0, 0.0001) 0.33%, rgba(0, 0, 0, 0.599049) 99.79%);"
+      },
+      fontFamily:{
+        mainFont:"Be Vietnam Pro"
+      },
+      boxShadow:{
+        btnshadow:" 0px 15px 15px -10px #FF9F8E"
+      },
+      fontSize:{
+        heroFontsize:" 40px",
+        heroFont:"56px"
+      },
+      width :{
+        imgSize:"540px",
+        heroText:"500px",
+        card:"385px"
+      },
+      objectPosition:{
+        centerCard:"50%"
+      },
+      backgroundPosition: {
+        calc: 'calc(50% + 200px) calc(50% - 1250px)',
+        calcBIg: 'calc(50% + 480px) calc(50% - 950px)',
+      },
+      backgroundSize: {
+        'mainBg-size': '606px',
+        'mainBg-sizeBig': '906px',
+      }
+
+    }
+  },
+  plugins: [],
+}
